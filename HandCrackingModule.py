90fa592a708a5cacbd830730b56648ab5abd59ab
eya kobbi <eya.kobbi@ensi-uma.tn>
2024-08-24 00:44:06 +0200

    first commit

--- HandCrackingModule.py
+++ HandCrackingModule.py
@@ -41,7 +41,7 @@ class handDetector():
                 print(id, cx, cy)
                 lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+                    cv2.circle(img,(cx,cy),7,(255,0,0),cv2.FILLED)
 
         return lmList
 
